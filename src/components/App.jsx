5e534d077ced6adb8148baa91b1f3ce9ca660441
TarasKupchenko <133959505+TarasKupchenko@users.noreply.github.com>
2024-02-04 23:44:11 +0200

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,10 @@
 import React, { lazy, Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import { ContactForm } from './ContactForm/ContactForm';
-import { ContactList } from './ContactList/ContactList';
-import { Filter } from './Filter/Filter';
-import { useAddContactHandler } from '../redux/ContactFormHandlers';
-import { useDeleteContactHandler } from '../redux/ContactListHandlers';
-import { useChangeFilterHandler } from '../redux/FilterHandlers';
-import { useFilteredContacts } from '../redux/selectors';
+
 import Navigation from './Navigation/Navigation';
 import css from './App.module.css';
 
 export const App = () => {
-  const addContactHandler = useAddContactHandler();
-  const deleteContactHandler = useDeleteContactHandler();
-  const changeFilterHandler = useChangeFilterHandler();
-  const filteredContacts = useFilteredContacts();
   const LoginPage = lazy(() => import('../page/Login'));
   const RegisterPage = lazy(() => import('../page/Register'));
   const ContactsPage = lazy(() => import('../page/Contacts'));
@@ -24,14 +14,7 @@ export const App = () => {
       <Suspense fallback={<div>Loading Navigation...</div>}>
         <Navigation />
       </Suspense>
-      <h1>Phonebook</h1>
-      <ContactForm onSubmit={addContactHandler} />
-      <h2>Contacts</h2>
-      <Filter onChange={changeFilterHandler} />
-      <ContactList
-        contacts={filteredContacts}
-        onDeleteContact={deleteContactHandler}
-      />
+
       <Suspense fallback={<div>Loading Contacts...</div>}>
         <Routes>
           <Route path="/login" element={<LoginPage />} />
--- src/page/Contacts.jsx
+++ src/page/Contacts.jsx
@@ -1,27 +1,31 @@
-import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { fetchContacts } from '../redux/contactsApi'; // Замініть на правильний шлях
+// ContactsPage.jsx
+import React from 'react';
+import { ContactForm } from '../components/ContactForm/ContactForm';
+import { ContactList } from '../components/ContactList/ContactList';
+import { Filter } from '../components/Filter/Filter';
+import { useAddContactHandler } from '../redux/ContactFormHandlers';
+import { useDeleteContactHandler } from '../redux/ContactListHandlers';
+import { useChangeFilterHandler } from '../redux/FilterHandlers';
+import { useFilteredContacts } from '../redux/selectors';
 
-const Contacts = () => {
-  const dispatch = useDispatch();
-  const contacts = useSelector(state => state.contacts.items);
-
-  useEffect(() => {
-    dispatch(fetchContacts());
-  }, [dispatch]);
+const ContactsPage = () => {
+  const addContactHandler = useAddContactHandler();
+  const deleteContactHandler = useDeleteContactHandler();
+  const changeFilterHandler = useChangeFilterHandler();
+  const filteredContacts = useFilteredContacts();
 
   return (
-    <div>
+    <>
+      <h1>Phonebook</h1>
+      <ContactForm onSubmit={addContactHandler} />
       <h2>Contacts</h2>
-      <ul>
-        {contacts.map(contact => (
-          <li key={contact.id}>
-            {contact.name}: {contact.number}
-          </li>
-        ))}
-      </ul>
-    </div>
+      <Filter onChange={changeFilterHandler} />
+      <ContactList
+        contacts={filteredContacts}
+        onDeleteContact={deleteContactHandler}
+      />
+    </>
   );
 };
 
-export default Contacts;
+export default ContactsPage;
